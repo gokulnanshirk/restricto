56d44789cc3538f0b330ed626dd94bee2f9714fb
Gokul Gr <51953306+gokulnanshirk@users.noreply.github.com>
2021-06-10 22:00:33 +0530

    Update home.page.ts

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,5 +8,20 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   constructor() {}
+  
+  login() { 
+    if(this.userld && this.password) {
+    if (this.userld == 'moni' && this.password == 'moni') {
+        this.navCtrl.navigateForward('home')
+    } else {
+        this.presentToast('Invalid userld or password.')
+    }
+   } else {
+    this.presentToast('Please provide Userld and Password.')
+   } 
+   } 
+async presentToast(message) { 
+    const toast = await this.toastController.create({ message: message, duration: 2000); toast.present(); 
+    }
 
 }
